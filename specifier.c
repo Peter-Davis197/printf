ec094e6ea8e9d28fbccd63cd50934bbd3d265ada
clairebuthelezi <aphiweclaire@gmail.com>
2023-06-27 17:37:03 -0700

    specifier.c

--- specifier.c
+++ specifier.c
@@ -3,41 +3,16 @@
 #include "main.h"
 
 /**
- * printAddress - Prints the memory address.
- * @ptr: Pointer to a memory location.
- */
-
-void printAddress(const void *ptr);
-/**
  * main - Entry point of the program.
  *
  * Return: Always 0.
  */
-
-int main(void);
-
-/**
- * printAddress - Prints the memory address.
- * @ptr: Pointer to a memory location.
- */
-
-void printAddress(const void *ptr)
-{
-	printf("The address of 'num' is: %p\n", ptr);
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- */
-
 int main(void)
 {
 	int num = 42;
-	int *ptr = &num;
 
-	printAddress((const void *)ptr);
+	printf("Decimal representation: %d\n", num);
+	printf("Integer representation: %i\n", num);
 
 	return (0);
 }
